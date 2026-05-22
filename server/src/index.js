464a60a2bb116d1949da0040c628830a5c40730d
Sunair7 <sunairsaleem00@gmail.com>
2026-05-22 15:52:29 +0500

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,9 @@
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import app from './app.js';
-import connectDB from './db/connect.js'; 
+import connectDB from './config/db.js';
+import cors from "cors";
+
 const PORT = Number(process.env.PORT) || 5000;
 const uri = process.env.MONGODB_URI;
 
@@ -14,4 +16,9 @@ if (!process.env.JWT_SECRET) {
   process.exit(1);
 }
 
+app.use(cors({
+  origin: "*"
+}));
+
+// Connect to Database
 connectDB();
