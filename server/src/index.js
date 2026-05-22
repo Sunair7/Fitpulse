87df64993770682a92169a54c4be6ba6f1fbc4a3
Sunair7 <sunairsaleem00@gmail.com>
2026-05-22 17:14:21 +0500

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import mongoose from 'mongoose';
 import app from './app.js';
 import connectDB from './config/db.js';
-import cors from "cors";
 
 const PORT = Number(process.env.PORT) || 5000;
 const uri = process.env.MONGODB_URI;
@@ -15,17 +14,6 @@ if (!process.env.JWT_SECRET) {
   console.error('Missing JWT_SECRET');
   process.exit(1);
 }
-
-app.use(
-  cors({
-    origin: [
-      'http://localhost:5173',
-      'https://fitpulse-lime.vercel.app'
-    ],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  })
-);
 
 // Connect to Database
 connectDB();
