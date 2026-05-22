2899a5f51b0af3d1b7614ed35e59882dba6457b2
Sunair7 <sunairsaleem00@gmail.com>
2026-05-22 15:59:17 +0500

    Update db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,12 +1,11 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB Atlas Connected Successfully');
     } catch (err) {
         console.error('Error connecting to MongoDB Atlas:', err);
@@ -14,4 +13,4 @@ const connectDB = async () => {
     }
 };
 
-export default connectDB;   
+export default connectDB;
